bdcfb355b646d6a1a135570eceefeaa46510e156
kidthales <kidthales@agogpixel.com>
2025-02-26 21:33:27 -0800

    Convert security config from yaml to php.

---
+++ config/packages/security.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $container): void {
+    $container->extension('security', [
+        'providers' => [
+            'users_in_memory' => [
+                'memory' => null,
+            ],
+        ],
+        'firewalls' => [
+            'dev' => [
+                'pattern' => '^/(_(profiler|wdt)|css|images|js)/',
+                'security' => false,
+            ],
+            'main' => [
+                'lazy' => true,
+                'provider' => 'users_in_memory',
+            ],
+        ],
+        'access_control' => null,
+    ]);
+};
