d96b87226c9ddc19866f750cfad7d1c1824d7b6e
kidthales <kidthales@agogpixel.com>
2025-03-02 14:30:33 -0800

    Install deps for Discord OAuth2 support.

--- config/packages/security.php
+++ config/packages/security.php
@@ -7,23 +7,27 @@ use App\Security\DiscordRequestAuthenticator;
 use Symfony\Config\SecurityConfig;
 
 return static function (SecurityConfig $security): void {
-    $memoryProvider = $security->provider('app_agent_user_provider')->memory();
-
+    $memoryProvider = $security->provider('app_agent_provider')->memory();
     $memoryProvider->user(DiscordRequestAuthenticator::AGENT_USER_IDENTIFIER)
         ->roles([Role::Agent->value]);
+
+    $security->provider('app_user_provider')->entity()
+        ->class(App\Entity\User::class)
+        ->property('discordId');
 
     $security->firewall('dev')
         ->pattern('^/(_(profiler|wdt)|css|images|js)/')
         ->security(false);
 
-    $security->firewall('discord')
+    $security->firewall('webhook_discord')
         ->pattern('^/webhook/discord')
         ->stateless(true)
-        ->provider('app_agent_user_provider')
+        ->provider('app_agent_provider')
         ->customAuthenticators([DiscordRequestAuthenticator::class]);
 
     $security->firewall('main')
-        ->lazy(true);
+        ->lazy(true)
+        ->provider('app_user_provider');
 
     $security->roleHierarchy(Role::Admin->value, [Role::User->value]);
     $security->roleHierarchy(Role::SuperAdmin->value, [Role::Admin->value]);
