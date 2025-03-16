eff2813ea2d1e196fe207a3e49ca22f120e459d8
kidthales <kidthales@agogpixel.com>
2025-03-16 10:06:20 -0700

    Add comment block to unauthorized action exception.

--- src/Exception/UnauthorizedActionException.php
+++ src/Exception/UnauthorizedActionException.php
@@ -9,6 +9,11 @@ use Throwable;
 
 final class UnauthorizedActionException extends Exception
 {
+    /**
+     * @param string $message
+     * @param int $code
+     * @param Throwable|null $previous
+     */
     public function __construct(string $message = "Unauthorized", int $code = 0, ?Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
